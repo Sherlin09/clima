069713e8465b50512c1c31b6269c3ea3fb189592
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-09-15 16:58:30 -0500

    validacion del formalario

--- js/app.js
+++ js/app.js
@@ -8,4 +8,36 @@ window.addEventListener('load', () => {
 
 function buscarClima(e) {
     e.preventDefault();
+
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligartorios');
+
+        return;
+    }
+}
+
+function mostrarError(mensaje){
+    const alerta = document.querySelector('.bd-red-100');
+
+    if(!alerta) {
+        const alerta = document.createElement('div');
+
+    alerta.classList.add('bd-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded',
+    'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+    alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block">${mensaje}</span>
+    `;
+    container.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 5000);
+    }
+
+
 }
